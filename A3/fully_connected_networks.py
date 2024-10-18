f3eaf1a159f32ea55e5ca29bbd2e736b2f0aaf1e
sui1234bian <2481364951@qq.com>
2024-10-18 16:09:59 +0800

    a

--- A3/fully_connected_networks.py
+++ A3/fully_connected_networks.py
@@ -436,30 +436,38 @@ class FullyConnectedNet(object):
     # dropout forward pass.                                                    #
     ############################################################################
     # Replace "pass" statement with your code
+    # 问题出在这段代码
+    # 问题：多了一个relu
+    # {linear - relu - [dropout]} x (L - 1) - linear - softmax
     cache = {}
     cache['h0'] = X.view(X.shape[0], -1)
-    for i in range(self.num_layers):
+    for i in range(self.num_layers-1):
       cache[f'h{i+1}'] = torch.mm(cache[f'h{i}'], self.params[f"W{i+1}"]) + self.params[f"b{i+1}"]
       cache[f'h{i+1}'] = torch.clamp(cache[f'h{i+1}'], min=0)
       if self.use_dropout:
-        pass
-    scores = cache[f'h{self.num_layers}']
+        cache[f'h{i+1}'], cache[f'dropout{i+1}'] = Dropout.forward(cache[f'h{i+1}'], self.dropout_param)
+    scores = torch.mm(cache[f'h{self.num_layers-1}'], self.params[f"W{self.num_layers}"]) + self.params[f"b{self.num_layers}"]
     scores -= torch.max(scores, dim=1)[0].view(scores.shape[0], -1)
     scores = torch.exp(scores)
     scores /= torch.sum(scores, dim=1).view(scores.shape[0], -1)
-    '''
+    
     # pass
-    h1 = torch.mm(X.view(X.shape[0], -1), self.params["W1"]) + self.params["b1"]
-    h1 = torch.clamp(h1, min=0)
-    # dropout
-    h2 = torch.mm(h1, self.params["W2"]) + self.params["b2"]
-    h2 = torch.clamp(h2, min=0)
-    # dropout
-    scores = torch.mm(h2, self.params["W3"]) + self.params["b3"]
-    scores -= torch.max(scores, dim=1)[0].view(scores.shape[0], -1)
-    scores = torch.exp(scores)
-    scores /= torch.sum(scores, dim=1).view(scores.shape[0], -1)
-    '''
+    # h1 = torch.mm(X.view(X.shape[0], -1), self.params["W1"]) + self.params["b1"]
+    # h1 = torch.clamp(h1, min=0)
+    # # dropout
+    # h2 = torch.mm(h1, self.params["W2"]) + self.params["b2"]
+    # h2 = torch.clamp(h2, min=0)
+    # # dropout
+    # scores = torch.mm(h2, self.params["W3"]) + self.params["b3"]
+    # scores -= torch.max(scores, dim=1)[0].view(scores.shape[0], -1)
+    # scores = torch.exp(scores)
+    # scores /= torch.sum(scores, dim=1).view(scores.shape[0], -1)
+    # cache = {}
+    # cache['h0'] = X.view(X.shape[0], -1)
+    # cache['h1'] = h1
+    # cache['h2'] = h2
+    # cache['h3'] = scores
+    # print(scores)
 
     ############################################################################
     #                             END OF YOUR CODE                             #
@@ -485,11 +493,19 @@ class FullyConnectedNet(object):
     loss /= scores.shape[0]
     for i in range(self.num_layers):
       loss += self.reg * torch.sum(self.params[f'W{i+1}'] * self.params[f'W{i+1}'])
+    # {linear - relu - [dropout]} x (L - 1) - linear - softmax  
     dscores = scores.clone() # (N*C)
     dscores[torch.arange(0, X.shape[0], 1), y] -= 1 # 选中的那项
     dscores /= X.shape[0]
-    for i in range(self.num_layers, -1, -1):
-      grads[f'W{i}'] = 
+    for i in range(self.num_layers, 0, -1):
+      grads[f'W{i}'] = torch.mm(cache[f'h{i-1}'].t(), dscores) + 2 * self.reg * self.params[f'W{i}']
+      grads[f'b{i}'] = torch.sum(dscores, dim=0)
+      dscores = torch.mm(dscores, self.params[f'W{i}'].t())
+      if self.use_dropout and i > 1:
+        dropout_cache = cache[f'dropout{i-1}']  # 获取当前层的 dropout 掩码
+        dscores = Dropout.backward(dscores, dropout_cache)
+      dscores[cache[f'h{i-1}'] <= 0] = 0
+      
     '''
     loss = torch.sum(-torch.log(scores[torch.arange(0,scores.shape[0] ,1), y]))
     loss /= scores.shape[0]
@@ -499,19 +515,24 @@ class FullyConnectedNet(object):
     dscores[torch.arange(0, X.shape[0], 1), y] -= 1 # 选中的那项
     dscores /= X.shape[0]
     '''
-    # '''
+    '''
+    
     # 第三层梯度
-    grads['W3'] = torch.mm(cache['h2'].t(), dscores) + 2 * self.reg * self.params['W3'] # (D, N) * (N, C) -> (D, C)
+    # grads['W3'] = torch.mm(cache['h2'].t(), dscores) + 2 * self.reg * self.params['W3'] # (D, N) * (N, C) -> (D, C)
+    grads['W3'] = torch.mm(h2.t(), dscores) + 2 * self.reg * self.params['W3'] # (D, N) * (N, C) -> (D, C)
     grads['b3'] = torch.sum(dscores, dim=0)
     # 第三层relu
     dh2 = torch.mm(dscores, self.params['W3'].t())
-    dh2[cache['h2'] <= 0] = 0
+    # dh2[cache['h2'] <= 0] = 0
+    dh2[h2 <= 0] = 0
     # 第二层梯度
-    grads['W2'] = torch.mm(cache['h1'].t(), dh2) + 2 * self.reg * self.params['W2'] # (D, N) * (N, C) -> (D, C)
+    # grads['W2'] = torch.mm(cache['h1'].t(), dh2) + 2 * self.reg * self.params['W2'] # (D, N) * (N, C) -> (D, C)
+    grads['W2'] = torch.mm(h1.t(), dh2) + 2 * self.reg * self.params['W2'] # (D, N) * (N, C) -> (D, C)
     grads['b2'] = torch.sum(dh2, dim=0)
     # 第二层relu
     dh1 = torch.mm(dh2, self.params['W2'].t())
-    dh1[cache['h1'] <= 0] = 0
+    # dh1[cache['h1'] <= 0] = 0
+    dh1[h1 <= 0] = 0
     # 第一层梯度
     grads['W1'] = torch.mm(X.t(), dh1) + 2 * self.reg * self.params['W1']
     grads['b1'] = torch.sum(dh1, dim=0)
@@ -559,13 +580,15 @@ def get_three_layer_network_params():
 
 def get_five_layer_network_params():
   ############################################################################
-  # TODO: Change weight_scale and learning_rate so your model achieves 100%  #
+  # Change weight_scale and learning_rate so your model achieves 100%  #
   # training accuracy within 20 epochs.                                      #
   ############################################################################
-  learning_rate = 2e-3  # Experiment with this!
-  weight_scale = 1e-5   # Experiment with this!
+  # learning_rate = 2e-3  # Experiment with this!
+  # weight_scale = 1e-5   # Experiment with this!
   # Replace "pass" statement with your code
-  pass
+  # pass
+  learning_rate = 1e-1  
+  weight_scale = 1e-1
   ############################################################################
   #                             END OF YOUR CODE                             #
   ############################################################################
@@ -601,11 +624,13 @@ def sgd_momentum(w, dw, config=None):
 
   next_w = None
   #############################################################################
-  # TODO: Implement the momentum update formula. Store the updated value in   #
+  # Implement the momentum update formula. Store the updated value in   #
   # the next_w variable. You should also use and update the velocity v.       #
   #############################################################################
   # Replace "pass" statement with your code
-  pass
+  # pass
+  v = config['momentum'] * v - config['learning_rate'] * dw
+  next_w = w + v
   #############################################################################
   #                              END OF YOUR CODE                             #
   #############################################################################
@@ -632,12 +657,13 @@ def rmsprop(w, dw, config=None):
 
   next_w = None
   ###########################################################################
-  # TODO: Implement the RMSprop update formula, storing the next value of w #
+  # Implement the RMSprop update formula, storing the next value of w #
   # in the next_w variable. Don't forget to update cache value stored in    #
   # config['cache'].                                                        #
   ###########################################################################
   # Replace "pass" statement with your code
-  pass
+  config['cache'] = config["decay_rate"] * config['cache'] + (1 - config["decay_rate"]) * dw * dw
+  next_w = w - config['learning_rate'] * dw / (config['cache'].sqrt() + config['epsilon'])
   ###########################################################################
   #                             END OF YOUR CODE                            #
   ###########################################################################
@@ -668,7 +694,7 @@ def adam(w, dw, config=None):
 
   next_w = None
   #############################################################################
-  # TODO: Implement the Adam update formula, storing the next value of w in   #
+  # Implement the Adam update formula, storing the next value of w in   #
   # the next_w variable. Don't forget to update the m, v, and t variables     #
   # stored in config.                                                         #
   #                                                                           #
@@ -676,7 +702,13 @@ def adam(w, dw, config=None):
   # using it in any calculations.                                             #
   #############################################################################
   # Replace "pass" statement with your code
-  pass
+  # pass
+  config['t'] += 1
+  config['m'] = config['beta1'] * config['m'] + (1 - config['beta1']) * dw
+  config['v'] = config['beta2'] * config['v'] + (1 - config['beta2']) * dw * dw
+  m_unbias = config['m'] / (1 - config['beta1'] ** config['t'])
+  v_unbias = config['v'] / (1 - config['beta2'] ** config['t'])
+  next_w = w - config['learning_rate'] * m_unbias / (v_unbias.sqrt() + config['epsilon'])
   #############################################################################
   #                              END OF YOUR CODE                             #
   #############################################################################
@@ -717,20 +749,23 @@ class Dropout(object):
 
     if mode == 'train':
       ###########################################################################
-      # TODO: Implement training phase forward pass for inverted dropout.       #
+      # Implement training phase forward pass for inverted dropout.       #
       # Store the dropout mask in the mask variable.                            #
       ###########################################################################
       # Replace "pass" statement with your code
-      pass
+      # pass
+      mask = (torch.rand(x.shape) > p).to(x.dtype).cuda()  # 掩码：大于 p 的值为 1，其他为 0
+      out = x * mask / (1 - p)
       ###########################################################################
       #                             END OF YOUR CODE                            #
       ###########################################################################
     elif mode == 'test':
       ###########################################################################
-      # TODO: Implement the test phase forward pass for inverted dropout.       #
+      # Implement the test phase forward pass for inverted dropout.       #
       ###########################################################################
       # Replace "pass" statement with your code
-      pass
+      # pass
+      out = x
       ###########################################################################
       #                             END OF YOUR CODE                            #
       ###########################################################################
@@ -753,10 +788,11 @@ class Dropout(object):
     dx = None
     if mode == 'train':
       ###########################################################################
-      # TODO: Implement training phase backward pass for inverted dropout       #
+      # Implement training phase backward pass for inverted dropout       #
       ###########################################################################
       # Replace "pass" statement with your code
-      pass
+      # pass
+      dx = dout * mask / (1 - dropout_param['p']) # 使用前向时的掩码，并进行相应缩放
       ###########################################################################
       #                            END OF YOUR CODE                             #
       ###########################################################################
